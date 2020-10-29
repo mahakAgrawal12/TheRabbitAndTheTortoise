d975c2a41bd259789cd9504db4ca48089995ebc0
Dell <mahak.agrawal_cs19@gla.ac.in>
2020-10-29 16:42:57 +0530

    overidden the run() Method from the Thread class

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,5 +1,7 @@
 package thread.tortoise;
 
+import javax.swing.*;
+
 public class Tortoise extends Thread{
 
     private static final int MILESTONES = 5;
@@ -7,4 +9,45 @@ public class Tortoise extends Thread{
     public Tortoise() {
         super("Tortoise");
     }
+
+    @Override
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    System.out.println("The Tortoise is leading and hence is going to sleep!");
+                    try {
+                        Thread.sleep(9000L);
+                    } catch (InterruptedException e) {
+                        System.err.println("The Tortoise's sleep got interrupted!");
+                    }
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                System.err.println("The Tortoise has lost the path!");
+            }
+        }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Tortoise has completed the race!",
+                "Tortoise",
+                JOptionPane.OK_CANCEL_OPTION
+        );
+    }
+
 }
